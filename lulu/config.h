96d0f9855327a91a566c15b77f406e30ad7d0219
Charles Stein <charles.stein@bitso.com>
2023-03-31 14:49:53 -0300

    turn off rgb after idle

--- lulu/config.h
+++ lulu/config.h
@@ -6,3 +6,6 @@
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define TAPPING_FORCE_HOLD
 #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 90
+#define RGBLIGHT_SLEEP 
+#define RGB_DISABLE_TIMEOUT 900000 // milliseconds
+#define RGB_DISABLE_WHEN_USB_SUSPENDED 1

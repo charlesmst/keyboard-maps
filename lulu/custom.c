9830384adff840a894635a148b01aea5608c3278
Charles Stein <charles.stein@bitso.com>
2023-03-31 23:53:29 -0300

    fix turned off rgb

--- lulu/custom.c
+++ lulu/custom.c
@@ -145,9 +145,7 @@ void process_platform_combo(uint16_t keycode, keyrecord_t *record) {
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
   if (host_keyboard_led_state().caps_lock) {
     RGB_MATRIX_INDICATOR_SET_COLOR(23, 255, 0, 0);
-  } else {
-    RGB_MATRIX_INDICATOR_SET_COLOR(23, 0, 0, 0);
-  }
+  } 
   return false;
 }
 

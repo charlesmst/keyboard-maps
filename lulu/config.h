63016847c6b65c2cc4da2e423b64ecfd1d8a3962
Charles Stein <charles@CHARLES-PC.localdomain>
2023-04-04 22:16:42 -0300

    upgrade qmk config

--- lulu/config.h
+++ lulu/config.h
@@ -1,10 +1,11 @@
 #pragma once
 #define RGB_MATRIX_LED_COUNT 70
 
-#define IGNORE_MOD_TAP_INTERRUPT
+// #define IGNORE_MOD_TAP_INTERRUPT
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define TAPPING_FORCE_HOLD
+// #define TAPPING_FORCE_HOLD
+#define QUICK_TAP_TERM 0
 #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 90
 #define RGBLIGHT_SLEEP 
 #define RGB_DISABLE_TIMEOUT 900000 // milliseconds

791c34405e23c9e81c64f779b6334f0290ccbecc
Charles Stein <charles.stein@bitso.com>
2023-04-14 00:02:54 -0300

    lily with encoder and update

---
+++ lily58/config.h
@@ -0,0 +1,14 @@
+
+#pragma once
+
+#define DIODE_DIRECTION COL2ROW
+
+// Use the lily version to get the Lily58 logo instead of the qmk logo
+#define OLED_FONT_H "lib/glcdfont_lily.c"
+
+#define ENCODERS_PAD_A { F4 }
+#define ENCODERS_PAD_B { F5 }
+#define ENCODER_RESOLUTION 2
+
+#define QUICK_TAP_TERM 0
+#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
--- lily58/rules.mk
+++ lily58/rules.mk
@@ -1,3 +1,4 @@
 MOUSEKEY_ENABLE = yes
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 
+ENCODER_ENABLE = yes

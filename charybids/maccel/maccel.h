538e4437dec002dca3575f0006ba1d099699b47c
Charles Stein <charles.mst@gmail.com>
2024-12-22 09:05:38 -0300

    update maccel to use the same as the wireless, without scaling on dpi

--- charybids/maccel/maccel.h
+++ charybids/maccel/maccel.h
@@ -8,10 +8,9 @@
 #include "report.h"
 
 report_mouse_t pointing_device_task_maccel(report_mouse_t mouse_report);
-bool           process_record_maccel(uint16_t keycode, keyrecord_t *record, uint16_t scaling, uint16_t takeoff, uint16_t growth_rate, uint16_t offset, uint16_t limit);
+bool           process_record_maccel(uint16_t keycode, keyrecord_t *record, uint16_t toggle, uint16_t takeoff, uint16_t growth_rate, uint16_t offset, uint16_t limit);
 
 typedef struct _maccel_config_t {
-    float scaling;
     float growth_rate;
     float offset;
     float limit;
@@ -25,12 +24,11 @@ void maccel_enabled(bool enable);
 bool maccel_get_enabled(void);
 void maccel_toggle_enabled(void);
 
-float maccel_get_scaling(void);
 float maccel_get_takeoff(void);
 float maccel_get_growth_rate(void);
 float maccel_get_offset(void);
 float maccel_get_limit(void);
-void  maccel_set_scaling(float val);
+void  maccel_set_growth_rate(float val);
 void  maccel_set_growth_rate(float val);
 void  maccel_set_offset(float val);
 void  maccel_set_limit(float val);
